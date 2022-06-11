ff93c695033fa4bb86b0e72f9bbe820272e2f74e
Dmuskeeter <tdheeraj9a@gmail.com>
2022-06-11 18:21:40 +0530

    add member to sqlite db

--- sango/about/views.py
+++ sango/about/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import About
 
 def about(request):
-    template=loader.get_template('about.html')
-    return HttpResponse(template.render())
+    frnds=About.objects.all().values()
+    output=''
+    for x in frnds:
+        output+=x["firstname"]
+    return HttpResponse(f'<h1>{output[0:7]}<h1/>')
+
 
